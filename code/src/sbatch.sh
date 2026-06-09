b5768e212ab2f4f54fb2f33ccea40f29ffe44348
enricosavorgnan <enricosavorgnan@gmail.com>
2026-06-09 17:58:09 +0200

    code: changed config_lorenz.yaml

--- code/src/sbatch.sh
+++ code/src/sbatch.sh
@@ -9,10 +9,10 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --gres=gpu:1
-#SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=4
+## SBATCH --gres=gpu:1
+#SBATCH --mem=48G
+#SBATCH --time=08:00:00
 
 # ==============================================================================
 # ENVIRONMENT SETUP
